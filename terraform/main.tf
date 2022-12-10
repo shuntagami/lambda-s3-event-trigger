908676bb8a3d47ecd01278be6ca5b6aab1405de4
shuntagami <shuntagami23@gmail.com>
2022-12-10 17:18:02 +0900

    support metric alerm
    https://gkzz.dev/posts/metric-alarms-by-multiple-dimensions/

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,3 +92,62 @@ resource "aws_lambda_permission" "s3_permission_to_trigger_lambda" {
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.shuntagami-demo-data.arn
 }
+
+resource "aws_sns_topic" "this" {
+  name = "sns-for-lambda"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = "https"
+  endpoint  = "https://global.sns-api.chatbot.amazonaws.com"
+}
+
+module "metric_alarms" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarms-by-multiple-dimensions"
+  version = "~> 3.0"
+
+  alarm_description = "all-lambda-functions-alarm"
+  alarm_name        = "all-lambda-functions-alarm"
+
+  # 以下を参考に「メトリクスを集計する AWSサービス」を指定する
+  # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html
+  namespace = "AWS/Lambda"
+
+  metric_name = "Errors"
+
+  # メトリクスの統計(集計方法)
+  # 以下のいずれかを指定し、集計した値と `threshold` とを `comparison_operator` の比較方法で比較する。
+  # Average | Maximum | Minimum | SampleCount | Sum
+  statistic = "SampleCount"
+
+  # 閾値
+  threshold = 0
+  # 閾値と比較する回数
+  evaluation_periods = 1
+
+  # `evaluation_periods` 1回あたりの統計が適用される期間（秒単位）
+  # 有効な値は、10、30、60、および60の任意の倍数。
+  period = 60
+
+  # アラーム状態へ遷移させるか判定する基準
+  # `period` * `evaluation_periods` のあいだに `datapoints_to_alarm` 回だけ `threshold` を、
+  # `comparison_operator` で指定する比較方法で比較し、TRUE である場合、アラーム状態へ遷移
+  # 上記の場合、60秒 * 1 = 60秒のあいだに、閾値である1か、1以上のエラーが検知されたら、アラーム状態へ遷移となる。
+  datapoints_to_alarm = 1
+
+  # `threshold` との比較演算子であり、以下のいずれかを指定。
+  # GreaterThanThreshold, GreaterThanOrEqualToThreshold, LessThanThreshold, or LessThanOrEqualToThreshold
+  comparison_operator = "GreaterThanThreshold"
+
+  # AWS Lambda の場合、以下を参考に入力。
+  # https://docs.aws.amazon.com/lambda/latest/dg/monitoring-metrics.html
+  # ここで「複数の Lambda」を監視するように指定できる！！
+  dimensions = {
+    "lambda1" = {
+      FunctionName = local.function_name
+    }
+  }
+
+  alarm_actions = [aws_sns_topic.this.arn]
+}
